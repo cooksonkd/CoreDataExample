367ca2184551861a41b3042a013c498d2a456138
Keenan Cookson <cooksonkd@gmail.com>
2024-04-27 21:23:47 +0200

    Deleted storyboard. Set RecipeListViewController as initial view controller

---
+++ CoreDataExample/ViewControllers/RecipeListViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RecipeListViewController.swift
+//  CoreDataExample
+//
+//  Created by Keenan Cookson on 2024/04/27.
+//
+
+import UIKit
+
+class RecipeListViewController: UIViewController {
+    
+    init() {
+        super.init(nibName: "RecipeListViewController", bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
