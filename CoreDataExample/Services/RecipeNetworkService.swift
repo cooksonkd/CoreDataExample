543745d82f19d241d5a7327913f5b6845deec4b3
Keenan Cookson <cooksonkd@gmail.com>
2024-04-27 23:04:55 +0200

    App now retrieves recipes from an API

---
+++ CoreDataExample/Services/RecipeNetworkService.swift
@@ -0,0 +1,32 @@
+//
+//  RecipeNetworkService.swift
+//  CoreDataExample
+//
+//  Created by Keenan Cookson on 2024/04/27.
+//
+
+import Foundation
+
+// MARK: -
+class RecipeNetworkService {
+    
+    /// Fetches recipes from the api.
+    /// - Returns: Result<[Recipe], Error>
+    func fetchRecipes() async -> Result<[Recipe], Error> {
+        guard let url = URL(string: "https://dummyjson.com/recipes") else { return .failure(RecipeError.failedToGetRecipes) }
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let recipeResponse = try JSONDecoder().decode(RecipesResponse.self, from: data)
+            return .success(recipeResponse.recipes)
+        } catch {
+            return .failure(error)
+        }
+    }
+    
+}
+
+// MARK: -
+enum RecipeError: Error {
+    case failedToGetRecipes
+}
+
--- CoreDataExample/ViewControllers/RecipeListViewController.swift
+++ CoreDataExample/ViewControllers/RecipeListViewController.swift
@@ -9,8 +9,26 @@ import UIKit
 
 class RecipeListViewController: UIViewController {
     
+    let networkService: RecipeNetworkService = RecipeNetworkService()
+    var recipes = [Recipe]()
+    
     init() {
         super.init(nibName: "RecipeListViewController", bundle: nil)
+        Task {
+            let result = await self.networkService.fetchRecipes()
+            self.setRecipes(using: result)   
+            guard self.recipes.isEmpty == false else { return }
+            print(self.recipes)
+        }
+    }
+    
+    private func setRecipes(using result: Result<[Recipe], any Error>) {
+        switch result {
+        case .success(let recipes):
+            self.recipes = recipes
+        case .failure(let error):
+            print("Error: ", error)
+        }
     }
     
     required init?(coder: NSCoder) {
@@ -20,5 +38,5 @@ class RecipeListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
 }
